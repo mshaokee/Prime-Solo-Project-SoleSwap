1abd453e7559d5beb6995708f7ee554fcf6d9e3c
Shaokee Moua <m.shaokee@gmail.com>
2020-05-28 21:13:43 -0500

    userpage is now class component

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import { Link } from 'react-router-dom';
 
-// this could also be written with destructuring parameters as:
-// const UserPage = ({ user }) => (
-// and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
-  <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in" />
-  </div>
-);
 
-// Instead of taking everything from state, we just want the user info.
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({user}) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
-});
+class UserPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1 id="welcome">Welcome, {this.props.user.username}!</h1>
+        <p>Your ID is: {this.props.user.id}</p>
+        <Link to="/home"><LogOutButton className="log-in" /></Link>
+      </div>
+    )//end return
+  }//end render
+};//end class
 
-// this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(UserPage);
+
+const putPropsOnState = reduxState => ({ reduxState, user: reduxState.user });
+
+export default connect(putPropsOnState)(UserPage);
