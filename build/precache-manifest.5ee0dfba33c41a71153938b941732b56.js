a8bc7c87009932b85dc17c2f41a02755036c46c9
Shaokee Moua <m.shaokee@gmail.com>
2020-06-04 23:10:17 -0500

    changed title

---
+++ build/precache-manifest.5ee0dfba33c41a71153938b941732b56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d90ab62a23746c52cf9926377fe21e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57a29b2c3f9ae7177431",
+    "url": "/static/css/main.466a53cb.chunk.css"
+  },
+  {
+    "revision": "9ab1ac61dfdc9756dadd",
+    "url": "/static/js/2.b9b2809a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b9b2809a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a29b2c3f9ae7177431",
+    "url": "/static/js/main.65b9a2ba.chunk.js"
+  },
+  {
+    "revision": "4a7ad318c6feee48a9ad",
+    "url": "/static/js/runtime-main.b0d368f5.js"
+  }
+]);
